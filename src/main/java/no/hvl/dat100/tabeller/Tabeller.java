9bfa77ca51d8864df7d51ba360bff5e9f79e7ab4
BenjaminBakken <benjaminbakken95@gmail.com>
2025-09-23 14:17:00 +0200

    c, d, e

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,62 +5,69 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
+        System.out.print("["); // Printer start av tabell før første element
 		for (int i = 0; i < tabell.length; i++) { // Itererer gjennom tabell
 
-            if (i == 0) {
-                System.out.print("["); // Printer start av tabell før første element.
-            }
             if (i > 0) {
-                System.out.print(", "); // Printer mellomrom mellom element før alle andre element enn det første elementet.
+                System.out.print(", "); // Printer mellomrom mellom element før alle andre element enn det første elementet
             }
-            System.out.print(tabell[i]); // Printer selve elementet.
-            if (i == tabell.length - 1) {
-                System.out.println("]"); // Printer slutt av tabell etter siste element.
-            }
+            System.out.print(tabell[i]); // Printer selve elementet
         }
-
+        System.out.println("]"); // Printer slutt av tabell etter siste element
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-        String tabellInnhold = "";
+        String tabellInnhold = "[";
         for (int i = 0; i < tabell.length; i++) { // Itererer gjennom tabell
 
-            if (i == 0) {
-                tabellInnhold += "["; // Legger til start av tabell før første element i strengen.
-            }
             if (i > 0) {
-                tabellInnhold += ","; // Legger til mellomrom mellom element før alle andre element enn det første elementet i strengen.
+                tabellInnhold += ","; // Legger til mellomrom mellom element før alle andre element enn det første elementet i strengen
             }
-            tabellInnhold += Integer.toString(tabell[i]); // Legger til selve elementet i strengen.
-            if (i == tabell.length - 1) {
-                tabellInnhold += "]"; // Printer slutt av tabell etter siste element.
-            }
+            tabellInnhold += Integer.toString(tabell[i]); // Legger til selve elementet i strengen
         }
+        tabellInnhold += "]";
         return tabellInnhold;
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+        for (int i : tabell) { // Itererer gjennom tabell
+
+            sum += i; // Legger til verdi av hvert element til totalsum av alle elementer
+        }
+        return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+		boolean svar = false;
+        for (int i : tabell) { // Itererer gjennom tabell
 
+            if (i == tall) { // Sjekker element mot input-tall og endrer svar til true dersom de er like
+                svar = true;
+                break;
+            }
+
+        }
+        return svar;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int posisjon = -1;
+        for (int i = 0; i < tabell.length; i++) { // Itererer gjennom tabell
+
+            if (tabell[i] == tall) {
+                posisjon = i;
+            }
+        }
+        return posisjon;
 	}
 
 	// f)
