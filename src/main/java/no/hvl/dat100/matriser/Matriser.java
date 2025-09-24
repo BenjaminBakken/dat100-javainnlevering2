55c640375a291b4e4260488c2552decf35ad462b
BenjaminBakken <benjaminbakken95@gmail.com>
2025-09-24 16:29:20 +0200

    Alle oppgaver ferdige
    Gjort alle oppgaver til innlevering del 2. Kan nå bare sammenligne svar før vi sender inn.

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,20 +2,20 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
-	// a)
-	public static void skrivUt(int[][] matrise) {
-		
-		for (int[] rad : matrise) {       // Itererer gjennom rader i matrise
+    // a)
+    public static void skrivUt(int[][] matrise) {
+
+        for (int[] rad : matrise) {       // Itererer gjennom rader i matrise
             for (int element : rad) {     // Itererer gjennom elementer i rad
                 System.out.print(element + " "); // Skriver ut element og mellomrom
             }
             System.out.println(); // Linjeskifte
         }
         System.out.println(); // Ekstra linjeskifte for å skille flere matriser
-	}
+    }
 
-	// b)
-	public static String tilStreng(int[][] matrise) {
+    // b)
+    public static String tilStreng(int[][] matrise) {
 
         String matriseStreng = ""; // Oppretter tom strengvariabel for å legge til elementer fra matrise.
         for (int[] rad : matrise) { // Itererer gjennom rader i matrise
@@ -25,30 +25,30 @@ public class Matriser {
             matriseStreng += "\n"; // Legger til linjeskifte til streng
         }
         return matriseStreng;
-		
-	}
 
-	// c)
-	public static int[][] skaler(int tall, int[][] matrise) {
+    }
+
+    // c)
+    public static int[][] skaler(int tall, int[][] matrise) {
 
         // Variabler for antall rader og antall kolonner i matrise. Siden det er en matrise, kan jeg anta at alle rader
         // vil være like lange som den første (matrise[0].length), som vil gi antall kolonner.
-        int radNr = matrise.length;
-        int kolonneNr = matrise[0].length;
+        int rader = matrise.length;
+        int kolonner = matrise[0].length;
         // Matrise til å lagre produktene av input-tall og verdiene i input-matrise
-        int[][] skalertMatrise = new int[radNr][kolonneNr];
+        int[][] skalertMatrise = new int[rader][kolonner];
 
-        for (int rad = 0; rad < radNr; rad++) { // Itererer gjennom rader i matrise
-            for (int kolonne = 0; kolonne < kolonneNr; kolonne++) { // Itererer gjennom elementer i rad
+        for (int rad = 0; rad < rader; rad++) { // Itererer gjennom rader i matrise
+            for (int kolonne = 0; kolonne < kolonner; kolonne++) { // Itererer gjennom elementer i rad
 
                 skalertMatrise[rad][kolonne] = matrise[rad][kolonne] * tall; // Produkt av input-tall og verdi i matrise
             }
         }
         return skalertMatrise;
-	}
+    }
 
-	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
+    // d)
+    public static boolean erLik(int[][] a, int[][] b) {
 
         // Bruker forskjellige betingelsesuttrykk til å sjekke om matrisene er ulike, og vil returnere "false" med en
         // gang ulikhet er funnet. Når alt er sjekket og ingen ulikheter er funnet, vil metoden returnere "true".
@@ -66,20 +66,20 @@ public class Matriser {
                 }
             }
         }
-		return true;
-	}
-	
-	// e)
-	public static int[][] speile(int[][] matrise) {
+        return true;
+    }
+
+    // e)
+    public static int[][] speile(int[][] matrise) {
 
         // Variabler for antall rader og antall kolonner i matrise. Siden det er en matrise, kan jeg anta at alle rader
         // vil være like lange som den første (matrise[0].length), som vil gi antall kolonner.
-        int radNr = matrise.length;
-        int kolonneNr = matrise[0].length;
-        int[][] speilMatrise = new int[radNr][kolonneNr]; // Variabel for speilet matrise av input
+        int rader = matrise.length;
+        int kolonner = matrise[0].length;
+        int[][] speilMatrise = new int[rader][kolonner]; // Variabel for speilet matrise av input
 
-        for (int rad = 0; rad < radNr; rad++) { // Itererer gjennom rader i matrise
-            for (int kolonne = 0; kolonne < kolonneNr; kolonne++) { // Itererer gjennom elementer i rad
+        for (int rad = 0; rad < rader; rad++) { // Itererer gjennom rader i matrise
+            for (int kolonne = 0; kolonne < kolonner; kolonne++) { // Itererer gjennom elementer i rad
 
                 // For å speile matrisen legger vi inn verdien fra kolonneplassens tilsvarende radplass og radplassens
                 // tilsvarende kolonneplass. Eksempel: Verdi i rad 2 - kolonne 4, blir lagt i rad 4 - kolonne 2 i ny matrise.
@@ -87,14 +87,36 @@ public class Matriser {
             }
         }
         return speilMatrise;
-	
-	}
 
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
-	}
+    // f)
+    public static int[][] multipliser(int[][] a, int[][] b) {
+
+        int aRader = a.length, aKolonner = a[0].length; // Antall rader og kolonner i matrise a
+        int bRader = b.length, bKolonner = b[0].length; // Antall rader og kolonner i matrise b
+
+        // Variabel for matrisen som er produktet av a og b, definert med like mange rader som a og kolonner som b
+        int[][] produktMatrise = new int[aRader][bKolonner];
+
+        if (bRader != aKolonner) { // Sjekker om matriser kan multipliseres (antall kolonner i a må vere lik rader i b)
+            // Printer feilmelding og stopper metoden ved å returnere tom matrise.
+            System.out.print("Kan ikke multiplisere matriser. Antall kolonner i første matrise må være lik rader i andre matrise");
+            return produktMatrise;
+        }
+        // Itererer gjennom rader og kolonner i produktMatrise (like mange rader som a og kolonner som b).
+        for (int r = 0; r < aRader; r++) {
+            for (int k = 0; k < bKolonner; k++) { // r = valgt rad, k = valgt kolonne
+
+                // Where the magic happens: Oppretter variabler 'i' for å telle gjennom kolonner i matrise a, og 'j' for
+                // å telle gjennom rader i matrise b. Produktet av element i matrise a og speilet element i
+                // matrise b blir lagt til totalsummen som skal bli nytt element i gitt rad og kolonne i ny matrise.
+                for (int i = 0, j = 0; i < aKolonner && j < bRader; i++, j++) {
+
+                    produktMatrise[r][k] += a[r][i] * b[j][k];
+                }
+            }
+        }
+        return produktMatrise;
+    }
 }
--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -2,8 +2,8 @@ package no.hvl.dat100.tabeller;
 
 public class Tabeller {
 
-	// a)
-	public static void skrivUt(int[] tabell) {
+    // a)
+    public static void skrivUt(int[] tabell) {
 
         System.out.print("["); // Printer start av tabell før første element
 
@@ -15,10 +15,10 @@ public class Tabeller {
             System.out.print(tabell[i]); // Printer selve elementet
         }
         System.out.println("]"); // Printer slutt av tabell etter siste element
-	}
+    }
 
-	// b)
-	public static String tilStreng(int[] tabell) {
+    // b)
+    public static String tilStreng(int[] tabell) {
 
         String tabellInnhold = "[";
 
@@ -31,24 +31,24 @@ public class Tabeller {
         }
         tabellInnhold += "]";
         return tabellInnhold;
-	}
+    }
 
-	// c)
-	public static int summer(int[] tabell) {
+    // c)
+    public static int summer(int[] tabell) {
 
-		int sum = 0;
+        int sum = 0;
 
         for (int i : tabell) {
 
             sum += i; // Legger til verdi av hvert element til totalsum av alle elementer
         }
         return sum;
-	}
+    }
 
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
+    // d)
+    public static boolean finnesTall(int[] tabell, int tall) {
 
-		boolean svar = false;
+        boolean svar = false;
 
         for (int i : tabell) {
 
@@ -59,12 +59,12 @@ public class Tabeller {
 
         }
         return svar;
-	}
+    }
 
-	// e)
-	public static int posisjonTall(int[] tabell, int tall) {
+    // e)
+    public static int posisjonTall(int[] tabell, int tall) {
 
-		int posisjon = -1; // Variabel til posisjon, initialiserer til -1 som blir returverdi dersom tall ikke funnet.
+        int posisjon = -1; // Variabel til posisjon, initialiserer til -1 som blir returverdi dersom tall ikke funnet.
 
         for (int i = 0; i < tabell.length; i++) {
 
@@ -74,42 +74,42 @@ public class Tabeller {
             }
         }
         return posisjon;
-	}
+    }
 
-	// f)
-	public static int[] reverser(int[] tabell) {
+    // f)
+    public static int[] reverser(int[] tabell) {
 
         int lengde = tabell.length; // Variabel for tabell-lengde for å unngå å gjenta metodekall flere ganger.
-		int[] reversTabell = new int[lengde]; // Tabell for å putte inn verdier i motsatt rekkefølge.
+        int[] reversTabell = new int[lengde]; // Tabell for å putte inn verdier i motsatt rekkefølge.
 
         for (int i = 0; i < lengde; i++) {
 
-            reversTabell[lengde-1-i] = tabell[i]; // Begynner på siste posisjon i reversTabell, iterer bakover
+            reversTabell[lengde - 1 - i] = tabell[i]; // Begynner på siste posisjon i reversTabell, iterer bakover
         }                                             // og setter inn verdier fra tabell i motsatt rekkefølge
         return reversTabell;
-	}
+    }
 
-	// g)
-	public static boolean erSortert(int[] tabell) {
+    // g)
+    public static boolean erSortert(int[] tabell) {
 
-		boolean sortert = true; // Variabel for om tabell er sortert. Starter med true, og endres ved moteksempel.
+        boolean sortert = true; // Variabel for om tabell er sortert. Starter med true, og endres ved moteksempel.
 
         for (int i = 0; i < tabell.length - 1; i++) {
 
-            if (tabell[i] > tabell[i+1]) { // Setter sortert-variabel til false dersom et element i tabellen er større
+            if (tabell[i] > tabell[i + 1]) { // Setter sortert-variabel til false dersom et element i tabellen er større
                 sortert = false;           // enn elementet etter. Vil stoppe iterasjon på nest siste plass for å unngå
                 break;                     // å gå out-of-bounds.
             }
         }
         return sortert;
-	}
+    }
 
-	// h)
-	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+    // h)
+    public static int[] settSammen(int[] tabell1, int[] tabell2) {
 
         // Variabel for ny tabell, med lengde lik summen av lengden av input-tabeller
         int[] nyTabell = new int[tabell1.length + tabell2.length];
-		int indeks = 0; // Variabel for å holde telling på indeks i ny tabell.
+        int indeks = 0; // Variabel for å holde telling på indeks i ny tabell.
 
         // To for-løkker for å iterere gjennom og sette inn verdiene fra hver tabell
         for (int i : tabell1) {
@@ -122,5 +122,5 @@ public class Tabeller {
         }
         return nyTabell;
 
-	}
+    }
 }
